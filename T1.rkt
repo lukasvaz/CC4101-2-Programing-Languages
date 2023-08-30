ff8be6b589b07a77d79df186f9ce6720d055c87e
Lukas <lukas.vasquez.verdejo@gmail.com>
2023-08-30 11:25:32 -0400

    P1 a b c

--- T1.rkt
+++ T1.rkt
@@ -1,4 +1,64 @@
 #lang play
-;Ejercicio1
+; (provide Prop)
+; (provide ocurrences)
+;----------P1a)--------------------
+; gramatica:
+;   <Prop>:: (varp <value>)
+;            |(andp <var1> <var2>)
+;            |(orp <var1> <var2>)
+;            |(notp <var1> <var2>)
 
-; a)
+(deftype Prop
+  (varp value)
+  (andp var1 var2)
+  (orp var1 var2)
+  (notp var1))
+
+;----------P1b)--------------------
+; ocurrences:: Prop String -> Number
+; devuelve la cantidad de veces que una variable aparece en una proposicion
+(define (ocurrences prop s)
+  (match prop
+    [(varp value) (if (equal? value s) 1 0)]
+    [(notp v1) (ocurrences v1 s)]
+    [(andp v1 v2) (+ (ocurrences v1 s) (ocurrences v2 s))]
+    [(orp v1 v2) (+ (ocurrences v1 s) (ocurrences v2 s))]
+    )
+  )
+
+;----------P1c)--------------------
+; vars :: Prop  -> (Listof String)
+; devuelve una lista con los nombres de todas las variables  que ocurren en la proposicion (sin duplicados)
+(define (vars prop)
+  (match prop
+    [(varp value) (list value)]
+    [(notp v1) (vars v1)]
+    [(andp v1 v2) (remove-duplicates (append (vars v1) (vars v2)))]
+    [(orp v1 v2) (remove-duplicates (append (vars v1) (vars v2)))]
+    ) )
+
+;----------P1d)--------------------
+; all-enviroments :: (Listof Strings)  -> (Listof (Listof (Pair String Boolean)))
+; dada una lista de variables (sin duplicados) crea todos los ambientes de evaluacion posibles
+(define (all-enviroments lista)
+  (if (equal? (length lista) 0)
+      
+      (list (list))
+
+      (map (lambda (e)
+            (list  (append (list (cons (car lista) #t)) e)
+             (append (list (cons (car lista ) #f)) e)
+             ))
+           (all-enviroments (cdr lista))
+           )
+
+      )
+  )
+
+
+; (equal? (all-enviroments (list "a")) (list
+;                                     (list (cons "a" #t))
+;                                     (list (cons "a" #f))
+;                                     ))
+
+(all-enviroments (list "a"))
--- test.rkt
+++ test.rkt
@@ -1,1 +1,20 @@
 #lang play
+(print-only-errors true)
+
+(require "T1.rkt")
+
+;P1a): damos un ejemplo de uso de la gramatica
+(orp (varp "a") (andp (varp "b") (notp (varp "c"))))
+
+; P1b)
+(test (ocurrences (varp "a") "b") 0 )
+(test (ocurrences (varp "a") "a") 1 )
+(test (ocurrences (andp (varp "a") (varp "b")) "a") 1 )
+(test (ocurrences (andp (varp "a") (varp "a")) "a") 2 )
+(test (ocurrences  (orp(andp (varp "b") (varp "b")) (notp (varp "b"))) "b" ) 3)
+(test (ocurrences  (orp(andp (varp "a") (varp "b")) (notp (varp "b"))) "b" ) 2)
+;P1c)
+(test (vars (varp "a")) (list "a"))
+(test (vars (andp (varp "a") (varp "b"))) (list "a" "b"))
+(test (vars (andp (varp "a")(varp "a")) ) (list "a"))
+(test (vars (andp(orp(andp (varp "a")(varp "a"))(varp "b"))(varp "c"))) (list "a" "b" "c"))
